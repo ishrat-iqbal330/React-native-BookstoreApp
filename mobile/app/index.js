04e19f6f701fa5f50c74d937ae3b817c3334b166
Ishrat Iqbal <contact2sketchesscout@gmail.com>
2025-06-05 11:36:45 +0500

    BS : mobile setup and CSS

---
+++ mobile/app/index.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>My first expo app</Text>
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1745253606009-9f01a3404db5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    color: "purple",
+    fontWeight: "bold",
+  },
+});
---
+++ mobile/constants/colors.js
@@ -0,0 +1,15 @@
+const COLORS = {
+    primary: "#6200ea",
+    textPrimary: "#212121",
+    textSecondary: "#757575",
+    textDark: "#424242",
+    placeholderText: "#bdbdbd",
+    background: "#fafafa",
+    cardBackground: "#ffffff",
+    inputBackground: "#f1f1f1",
+    border: "#e0e0e0",
+    white: "#ffffff",
+    black: "#000000",
+}
+
+export default COLORS;
